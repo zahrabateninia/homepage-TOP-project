c3f85f44fac83b8174005dcab90df830ca13df50
zahrabateninia <zahrabatenin@gmail.com>
2025-05-30 19:34:24 +0330

    Add custom cursor

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-menu = document.querySelector("#menu-bars");
-header = document.querySelector('header');
+let menu = document.querySelector("#menu-bars");
+let header = document.querySelector('header');
 
 menu.onclick = ()=>{
     menu.classList.toggle("fa-times")
@@ -10,3 +10,28 @@ window.onscroll = ()=>{
     menu.classList.remove("fa-times")
     header.classList.remove('active')
 }
+
+
+let cursor1 =  document.querySelector(".cursor-1")
+let cursor2 =  document.querySelector(".cursor-2")
+
+window.onmousemove = (e)=>{
+    cursor1.style.top= e.pageY + 'px'
+    cursor1.style.left= e.pageX + 'px'
+    cursor2.style.top= e.pageY + 'px'
+    cursor2.style.left= e.pageX + 'px'
+}
+
+
+document.querySelectorAll('a').forEach(links =>{
+
+    links.onmouseenter = ()=>{
+        cursor1.classList.add('active')
+        cursor2.classList.add('active')
+    }
+
+    links.onmouseleave = ()=>{
+        cursor1.classList.remove('active')
+        cursor2.classList.remove('active')
+    }
+})
