d7fabd8471948d6827137902281e917469368801
zahrabateninia <zahrabatenin@gmail.com>
2025-05-30 17:05:26 +0330

    fix: Link js correctly and handle clicking menu bars icon

---
+++ script.js
@@ -0,0 +1,12 @@
+menu = document.querySelector("#menu-bars");
+header = document.querySelector('header');
+
+menu.onclick = ()=>{
+    menu.classList.toggle("fa-times")
+    header.classList.toggle('active')
+}
+
+window.onscroll = ()=>{
+    menu.classList.remove("fa-times")
+    header.classList.remove('active')
+}
